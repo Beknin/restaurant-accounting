9ae83bf3d4341939a3abe9f9b5ae302adc2946b2
Beknin <iposohov1@gmail.com>
2026-06-06 01:34:14 +0800

    Changed smoke tests to work with pytest

--- tests/smoke/test_smoke.py
+++ tests/smoke/test_smoke.py
@@ -8,7 +8,8 @@ from tkinter import messagebox
 from unittest.mock import patch
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
-from packages.core.management import DatabaseManager, TABLE_CONFIG
+from packages.core.management import DatabaseManager
+from packages.core.db_structure import TABLE_CONFIG
 
 # ----------------------------------------------------------------------
 # Фикстуры
